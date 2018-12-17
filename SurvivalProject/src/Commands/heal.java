89ec054a94ef5a5cdc0b2979a7bdeea43842f03c
proll <proll@DESKTOP-R30TH34.fritz.box>
2018-12-17 20:54:05 +0100

    ,

--- SurvivalProject/src/Commands/heal.java
+++ SurvivalProject/src/Commands/heal.java
@@ -20,7 +20,7 @@ public class heal implements CommandExecutor{
 				if(args.length == 0) {
 					p.setHealth(20);
 					p.setFoodLevel(20);
-					p.sendMessage("§cYou were healed§7!");
+					p.sendMessage(Main.getInstance().prefix + "§cYou were healed§7!");
 					
 			
 				}else if(args.length == 1) {
@@ -28,22 +28,22 @@ public class heal implements CommandExecutor{
 					if(target != null) {
 						target.setHealth(20);
 						target.setFoodLevel(20);
-						target.sendMessage(Main.getInstance().prefix + "§cYou were healed§7!");
+						target.sendMessage(Main.getInstance().prefix + "§aYou were healed§7!");
 						if(target == p) {
 							
 							
 						}else {
-							target.sendMessage(Main.getInstance().prefix + "§cYou were healed§7!");
-							p.sendMessage(Main.getInstance().prefix + "§cYou healed " + target.getName() + " §7!");
+							target.sendMessage(Main.getInstance().prefix + "§aYou were healed§7!");
+							p.sendMessage(Main.getInstance().prefix + "§aYou healed " + target.getName() + " §7!");
 						}
 					}else {
-						p.sendMessage(Main.getInstance().prefix + "§cThe player is not online§7!");
+						p.sendMessage(Main.getInstance().prefix + "§aThe player is not online§7!");
 					}
 				}else if(args.length > 1) {
-					p.sendMessage(Main.getInstance().prefix + "§cPlease type §7/§cheal§7!");
+					p.sendMessage(Main.getInstance().prefix + "§aPlease type §7/§cheal§7!");
 				}	
 			}else {
-				p.sendMessage("§cYou have no Permission to do this§7!");
+				p.sendMessage(Main.getInstance().prefix + "§aYou have no Permission to do this§7!");
 			}
 
 		}
--- SurvivalProject/src/Commands/spawn.java
+++ SurvivalProject/src/Commands/spawn.java
@@ -1,11 +1,13 @@
 package Commands;
 
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import Config.Config;
+import Main.Main;
 
 public class spawn implements CommandExecutor {
 
@@ -17,6 +19,8 @@ public class spawn implements CommandExecutor {
 			Player p = (Player)cs;
 			
 			Config.teleportSpawn(p);
+			p.sendMessage(Main.getInstance().prefix + "§aYou were teleported to the spawn§7!");
+			p.playSound(p.getLocation(), Sound.LEVEL_UP, 1f, 1f);
 			
 		
 		}
